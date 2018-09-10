c9350157b9ed7ab77e0675c904efe1cc60674b3f
z1061227423 <1061227423@qq.com>
2018-09-10 10:56:00 +0800

    my first commit

---
+++ USB_Test/BubbleSort.cpp
@@ -0,0 +1,17 @@
+//√∞≈›≈≈–ÚÀ„∑®
+void BubbleSort(int arr[], int len)
+{
+	int i, j, temp;
+	for (i = 0; i < len - 1; i++)
+	{
+		for (j = 0; j < len - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				temp = arr[j + 1];
+				arr[j + 1] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+}
