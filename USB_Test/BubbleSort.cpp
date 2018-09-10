c27a4cfa3d81d9c96b7e083931f7895e792c2984
z1061227423 <1061227423@qq.com>
2018-09-10 11:10:59 +0800

    c=3_

--- USB_Test/BubbleSort.cpp
+++ USB_Test/BubbleSort.cpp
@@ -1,9 +1,9 @@
 //√∞≈›≈≈–ÚÀ„∑®
-void MyBubbleSort(int arr[], int len)
+void My_BubbleSort(int arr[], int len)
 {
 	int i, j, temp;
 	int flag = 0, a = 1;
-	int b = 2;
+	int b = 2, c = 3;
 	for (i = 0; i < len - 1; i++)
 	{
 		for (j = 0; j < len - i - 1; j++)
